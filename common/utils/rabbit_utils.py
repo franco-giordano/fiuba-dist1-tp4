2c600d63801499c322b7c2cc9c6f1850fdcd9d12
franco-giordano <francogior98@gmail.com>
2021-07-21 10:48:43 -0300

    habilitar por defecto auto_ack en rabbitutils

--- common/utils/rabbit_utils.py
+++ common/utils/rabbit_utils.py
@@ -8,7 +8,7 @@ class RabbitUtils:
         return conn, channel
 
     @staticmethod
-    def setup_input_direct_exchange(channel, exchange_name, routing_key, callback, queue_name=None):
+    def setup_input_direct_exchange(channel, exchange_name, routing_key, callback, queue_name=None, auto_ack=True):
         channel.exchange_declare(exchange=exchange_name, exchange_type='direct')
         if not queue_name:
             result = channel.queue_declare(queue='', exclusive=True)
@@ -16,19 +16,19 @@ class RabbitUtils:
         else:
             channel.queue_declare(queue=queue_name, exclusive=False)
         channel.queue_bind(exchange=exchange_name, queue=queue_name, routing_key=routing_key)
-        channel.basic_consume(queue=queue_name, on_message_callback=callback)
+        channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=auto_ack)
 
     @staticmethod
     def setup_output_direct_exchange(channel, exchange_name):
         channel.exchange_declare(exchange=exchange_name, exchange_type='direct')
 
     @staticmethod
-    def setup_input_fanout_exchange(channel, exchange_name, callback):
+    def setup_input_fanout_exchange(channel, exchange_name, callback, auto_ack=True):
         RabbitUtils.setup_fanout_exchange(channel, exchange_name)
         result = channel.queue_declare(queue='', exclusive=True)
         queue_name = result.method.queue
         channel.queue_bind(exchange=exchange_name, queue=queue_name)
-        channel.basic_consume(queue=queue_name, on_message_callback=callback)
+        channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=auto_ack)
 
     @staticmethod
     def setup_fanout_exchange(channel, exchange_name):
@@ -43,9 +43,9 @@ class RabbitUtils:
         channel.queue_declare(queue=queue_name)
 
     @staticmethod
-    def setup_input_queue(channel, queue_name, callback):
+    def setup_input_queue(channel, queue_name, callback, auto_ack=True):
         RabbitUtils.setup_queue(channel, queue_name)
-        channel.basic_consume(queue=queue_name, on_message_callback=callback)
+        channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=auto_ack)
 
     @staticmethod
     def send_to_queue(channel, queue_name, body, corr_id=None):
